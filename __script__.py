17e2f83655d2101f782226027429e7b2db32d248
Romy Yigitoglu <hannyapythies@gmail.com>
2021-11-16 09:24:58 +0100

    tst

--- __script__.py
+++ __script__.py
@@ -2,12 +2,8 @@
 
 monPremierCommit = 1
 maPremiereBranche = 0
-<<<<<<< HEAD
 maPremiereFusion = 1
-maPremiereErreur = 0
-=======
-maPremiereFusion = 0
 maPremiereErreur = 1
->>>>>>> Dev
+
 "jeVaisMalNommerMonProchaonCommit = 1" 
 unicormeMode = 1 
